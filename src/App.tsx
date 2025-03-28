dcb075485ad769a8c6ccb8311ca432d56f28b7ef
Luis Henrique Ramos Soares <89323852+luishh205@users.noreply.github.com>
2025-03-27 21:28:37 -0300

    Create Projetc

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import styles from "./App.module.css"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+      <Header/>
+        <main className={styles.main}><h1>Conteúdo...</h1></main>
+      <Footer/>
+   </div>
   );
 }
 
